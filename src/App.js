29aa559541fb72596d470b9d69ab88361f98594d
Greg <greg@guavatek.tech>
2020-04-21 07:58:47 +0800

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount(){
+    
+    fetch('https://www.breakingbadapi.com/api/quotes')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+  }
+  render() {
+      var{ isLoaded, items } = this.state;
+
+      if (!isLoaded) {
+        console.log(items)
+        return <div>Loading...</div>
+      }
+      else {
+      return(
+        <div class="App">
+            <ul>
+              {items.map(item =>(
+                  <li key={item.quote_id}>
+                    Quote: {item.quote} | Author {item.author}
+                  </li>
+              ))}
+            </ul>
+        </div>
+      );
+    }
+  }
+
 }
-
 export default App;
